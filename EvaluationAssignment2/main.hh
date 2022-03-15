4e4f5f21481f271193dd83a5a8569c067060cb47
Lucas Hamilton <me@lucashamilton.com>
2022-03-14 18:40:43 -0700

    convert the timing function into a class/object

--- EvaluationAssignment2/main.hh
+++ EvaluationAssignment2/main.hh
@@ -16,4 +16,17 @@ typedef int64_t int_t;
 
 #define NANOS 1000000000
 
-double timer(int_t length, double, int_t secondArraySize, int_t prefetch);
+class Timer{
+  public:
+    int_t length;
+    double searches;
+    int_t secondArraySize;
+    int_t prefetch;
+
+    Timer(int_t length, double searches,int_t secondArraySize, int_t prefetch) :
+    length(length), searches(searches), secondArraySize(secondArraySize), prefetch(prefetch){};
+
+    void operator() (double* answer);
+  };
+
+using namespace std;
