90a6ad65b9e801337234158354eaa81318cd7f07
Lucas Hamilton <me@lucashamilton.com>
2022-03-19 17:52:19 -0700

    update timing.cc

--- EvaluationAssignment2/timing.cc
+++ EvaluationAssignment2/timing.cc
@@ -8,11 +8,11 @@
     int i;
     int j;
     int_t tmp;
-    struct timespec start, finish, delta;
+      struct timespec start, finish, delta;
 
     random_device device;
     mt19937 generator(device());
-    uniform_int_distribution<int_t> distr(1,length);
+    uniform_int_distribution<int_t> distr(0,length-1);
     uniform_int_distribution<int_t> distr2(1,secondArraySize);
 
     vector<int_t> array(length,distr(generator));
@@ -28,7 +28,7 @@
     for(i = 0; i < searches; i++){
       if (secondArraySize >0){
         //this for loop could probably be optimized
-        for (j = 0; j < 50; j++){
+        for (j = 0; j < 10; j++){
           oppIndex = distr2(generator);
           tmp = array2[oppIndex];
         }
@@ -38,7 +38,7 @@
         tmp = array[index];
       }
       clock_gettime(CLOCK_REALTIME, &start);
-      //tmp = array[index];
+      tmp = array[index];
       clock_gettime(CLOCK_REALTIME, &finish);
       delta.tv_sec = finish.tv_sec - start.tv_sec;
       delta.tv_nsec = labs(finish.tv_nsec - start.tv_nsec);
@@ -56,32 +56,27 @@ int main(){
   // PARAMETERS alter the assignment of thee variabls to change the program:
     int_t initialLength = 100;
     int_t maxLength = 1000000000;
+    double searches = 10000000;
+
     int_t array2Length = 0;
     int_t prefetch = 0;
-    double searches = 10000000;
 
   int i;
   int_t numAccesses;
-  struct timespec start, end, delta;
+  //struct timespec start, end, delta;
 
   numAccesses = calculatepower(initialLength, maxLength);
   auto timer = Timer(initialLength, searches, array2Length, prefetch);
-
-printf("Buffer Size (bytes)     Completion Time (ns):\n");
-  clock_gettime(CLOCK_REALTIME, &start);
   vector<double> answerVect(numAccesses, 0);
-  for (i = 0; i < numAccesses ; i ++){
-    timer(&answerVect[i]);
-    printf("%lld                     %f ns\n",initialLength*sizeof(int_t), answerVect[i]);
-    initialLength *= 2;
-    timer.updateLength(initialLength);
+  printf("Buffer Size (bytes),Completion Time (ns)\n");
+  for (i=0;i<numAccesses;i++){
+    for (i = 0; i < numAccesses ; i ++){
+      timer(&answerVect[i]);
+      printf("%lld,%f\n",initialLength*sizeof(int_t), answerVect[i]);
+      initialLength *= 2;
+      timer.updateLength(initialLength);
+    }
   }
-  clock_gettime(CLOCK_REALTIME, &end);
-  delta.tv_sec = end.tv_sec - start.tv_sec;
-  delta.tv_nsec = end.tv_nsec - start.tv_nsec;
-  double time = (delta.tv_sec) + (delta.tv_nsec)/(double)NANOS;
-
-  printf("\nThe total Completion time was:  %f s\n",time);
   return 0;
 
 }
